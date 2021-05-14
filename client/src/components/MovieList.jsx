8f1e4a02c625b6d40edb163e1c055f7e55c0bd52
patrickrgrady81 <patrickrgrady81@gmail.com>
2021-05-14 15:11:20 -0400

    only show 5 cards

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -7,20 +7,38 @@ import Card from './Card'
 
 const MovieList = props => {
 
+  const[max, setMax] = useState(4)
+
+  const handleButtonLeft = () => {
+    if (!(max < 5)){
+      setMax(max - 1);
+    }
+  }
+
+  const handleButtonRight = () => {
+    if (!(max > 9)){
+      setMax(max + 1);
+    }
+  }
+
   if (props.movies){
     return(
       <>
-        <ul className="noBullets">
-          {props.movies.map(movie => {
+        <button onClick={handleButtonLeft}>&#60;  PREV </button>
+        <ul className="noBullets flex-container" >
+          {props.movies.map((movie, index) => {
+            if (index <= max && index > max-5) {
             return(
-              <div key={movie.id}>
-                <Card title={movie.title} date={movie.release_date} overview={movie.overview} poster={movie.poster_path}/>
-              </div>
-          )})}
+                <div key={movie.id} >
+                  <Card className="flex-item" title={movie.title} date={movie.release_date} overview={movie.overview} poster={movie.poster_path}/>
+                </div>
+          )}})}
         </ul>
-        {/* <h1>hi</h1> */}
-      </>
-    )} else {
+        <button onClick={handleButtonRight}> NEXT  &#62; </button>
+     </>
+    )
+    
+  } else {
       return(
         <>
           <h1>Loading...</h1>
